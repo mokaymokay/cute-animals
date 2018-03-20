8a4cb012aaaca312ec226b5d89f88ec50e2e9348
Kay Mok <kaymok3@gmail.com>
2018-03-20 13:39:46 -0400

    Exercise #3

--- Animals/scripts/main.js
+++ Animals/scripts/main.js
@@ -1,3 +1,18 @@
 // 1
 $('p:nth-child(2)').text("Animals Are Great!");
-$('p:nth-child(2)').addClass('sub-head');
+$('p:nth-child(2)').addClass("sub-head");
+
+// 2
+$('#logo').click( function(e) {
+  $(this).css("background-image", "url('../img/logos/puppy.png')")
+})
+
+// 3
+let div = $("<div></div>").addClass("cute-container");
+
+for (let i = 6; i > 0; i--) {
+  let cute = $("<div></div>").addClass("cute");
+  $('body').append(cute);
+}
+
+// 4
